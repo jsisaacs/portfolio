f79bf9913cd069b39518c6ebb0719863d262b669
Josh Isaacson <jsisaacs@iu.edu>
2018-12-20 07:48:52 -0500

    broke recent commits into a section, each event routes to the repo when clicked

--- js/recent-commits.js
+++ js/recent-commits.js
@@ -1,15 +1,72 @@
-const axios = require('axios');
+const recentCommitsDiv = document.getElementsByClassName('recent-commits')[0];
 
-axios.get('https://api.github.com/users/jsisaacs/events/public')
+const config = {
+    'user-agent': 'username: jsisaacs'
+  };
+
+axios.get('https://api.github.com/users/jsisaacs/events/public?access_token=2830290cc5daef3c9c05f872d05e38d91940b807')
     .then(function (response) {
         const data = response.data;
-        const mostRecentEvents = data.slice(0, 5);
+        const mostRecentEvents = [];
         
-        mostRecentEvents.map(event => {
-            console.log(event.repo.name);
-            console.log(event.payload.commits[0].message);
-            console.log(event.created_at);
-            console.log('---');
+        // get the 5 most recent PushEvents
+        data.map(event => {
+            if (event.type === 'PushEvent') {
+                mostRecentEvents.push(event);
+            }
+        });
+
+        filteredCommits = mostRecentEvents.slice(0, 5);
+        
+        filteredCommits.map(event => {
+            createdAt = new Date(event.created_at);
+
+            const commitLink = document.createElement('a');
+            commitLink.className = 'event-link';
+            commitLink.href = 'https://github.com/' + event.repo.name;
+
+            const eventLevel = document.createElement('div');
+            eventLevel.id = 'event-commit-level';
+            eventLevel.className = 'level is-mobile';
+
+            const levelItem1 = document.createElement('div');
+            levelItem1.className = 'level-item';
+
+            const levelItem2 = document.createElement('div');
+            levelItem2.className = 'level-item';
+
+            const eventDiv = document.createElement('div');
+            eventDiv.className = 'event-div';
+
+            const repoName = document.createElement('p');
+            repoName.className = 'event-repo';
+            repoName.innerText = event.repo.name;
+
+            const commitMessage = document.createElement('p');
+            commitMessage.className = 'event-message';
+            commitMessage.innerText = event.payload.commits[0].message;
+
+            const date = document.createElement('p');
+            date.className = 'event-date';
+            date.innerText = createdAt.toLocaleDateString();
+
+            const time = document.createElement('p');
+            time.className = 'event-time';
+            time.innerText = createdAt.toLocaleTimeString();
+
+            levelItem1.appendChild(repoName);
+            levelItem2.appendChild(date);
+            levelItem2.appendChild(time);
+
+            eventLevel.appendChild(levelItem1);
+            eventLevel.appendChild(levelItem2);
+
+            eventDiv.appendChild(eventLevel);
+            eventDiv.appendChild(commitMessage);
+
+            commitLink.appendChild(eventDiv);
+
+            recentCommitsDiv.appendChild(commitLink);
         });
     })
     .catch(function (error) {
